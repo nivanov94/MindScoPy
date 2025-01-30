957f8323836cd45efb6bbff17f53d4291b0b1895
nivanov <nicolas.ivanov94@gmail.com>
2025-01-29 21:15:35 -0500

    Preprocessing init.

---
+++ preprocessing/__init__.py
@@ -0,0 +1,4 @@
+from .artifact_removal import peak_rejection, riemannian_potato_rejection
+from .rebias import apply_rebias_to_groups
+from .misc import epoch, bandpass_filter, resample
+from .feature_extraction import ScaledTangentSpace
